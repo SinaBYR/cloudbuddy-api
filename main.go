21fce2cc73b641b09f77e121c5340668d221dd16
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2024-06-26 20:49:07 +0330

    fix(routes): read multipart file in uploadToBucket function

--- main.go
+++ main.go
@@ -24,10 +24,10 @@ import (
 )
 
 type Image struct {
-	UUID      string    `clover:"_id"`
-	Url       string    `clover:"url"`
-	Likes     int64     `clover:"likes"`
-	CreatedAt time.Time `clover:"created_at"`
+	UUID      string    `clover:"_id" json:"uuid"`
+	Url       string    `clover:"url" json:"image_url"`
+	Likes     int64     `clover:"likes" json:"likes"`
+	CreatedAt time.Time `clover,json:"created_at" json:"created_at"`
 }
 
 func main() {
@@ -132,9 +132,7 @@ func postImage(db *cl.DB) func(c *gin.Context) {
 			return
 		}
 
-		file.Filename = file.Filename + docId
-
-		err = uploadToBucket(file)
+		err = uploadToBucket(file, docId)
 		if err != nil {
 			log.Println(err)
 			innerErr := db.DeleteById("images", docId)
@@ -191,7 +189,8 @@ func postImage(db *cl.DB) func(c *gin.Context) {
 	}
 }
 
-func uploadToBucket(file *multipart.FileHeader) error {
+// prefix is basically an optional string which gets prepended to name of the file
+func uploadToBucket(file *multipart.FileHeader, prefix string) error {
 	err := godotenv.Load()
 	if err != nil {
 		return errors.New(fmt.Sprintf("Error loading environment variables: %s", err.Error()))
@@ -200,6 +199,7 @@ func uploadToBucket(file *multipart.FileHeader) error {
 	accessKey := os.Getenv("BUCKET_ACCESS_KEY")
 	secretKey := os.Getenv("BUCKET_SECRET_KEY")
 	bucketName := os.Getenv("BUCKET_NAME")
+	endpoint := os.Getenv("BUCKET_ENDPOINT")
 
 	if accessKey == "" || secretKey == "" || bucketName == "" {
 		return errors.New(fmt.Sprintf("Environment variables are not loaded correctly"))
@@ -207,6 +207,7 @@ func uploadToBucket(file *multipart.FileHeader) error {
 
 	sess, err := session.NewSession(&aws.Config{
 		Region:      aws.String("us-west-2"),
+		Endpoint:    aws.String(endpoint),
 		Credentials: credentials.NewStaticCredentials(accessKey, secretKey, ""),
 	})
 	if err != nil {
@@ -215,7 +216,7 @@ func uploadToBucket(file *multipart.FileHeader) error {
 
 	client := s3.New(sess)
 
-	f, err := os.Open(file.Filename)
+	f, err := file.Open()
 	defer f.Close()
 	if err != nil {
 		return errors.New(fmt.Sprintf("Error opening file: %s", err.Error()))
@@ -227,7 +228,12 @@ func uploadToBucket(file *multipart.FileHeader) error {
 		return errors.New(fmt.Sprintf("Error reading file: %s", err.Error()))
 	}
 
-	destinationKey := "cloudbuddy/" + file.Filename
+	destinationKey := strings.Join([]string{
+		"cloudbuddy/",
+		prefix,
+		"-",
+		file.Filename,
+	}, "")
 
 	// This uploads the contents of the buffer to S3
 	_, err = client.PutObject(&s3.PutObjectInput{
