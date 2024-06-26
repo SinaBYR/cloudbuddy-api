c4896ab1deed770d5279332599b8ae33873ed04e
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2024-06-26 15:14:49 +0330

    feat(routes): implement getImageById route

--- main.go
+++ main.go
@@ -30,8 +30,39 @@ func main() {
 	images := r.Group("/v1/images")
 
 	images.GET("/", getAllImages(db))
+	images.GET("/:id", getImageById(db))
 
 	r.Run()
+}
+
+func getImageById(db *cl.DB) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		doc, err := db.FindFirst(q.NewQuery("images").Where(q.Field("uuid").Eq(id)))
+
+		if doc == nil {
+			c.JSON(http.StatusNotFound, gin.H{
+				"message": "cloud not found :(",
+			})
+			return
+		}
+
+		if err != nil {
+			if err == cl.ErrDocumentNotExist {
+				c.JSON(http.StatusNotFound, gin.H{
+					"message": "cloud not found :(",
+				})
+			} else {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"message": "wtf",
+				})
+			}
+
+			return
+		}
+
+		c.JSON(http.StatusOK, doc)
+	}
 }
 
 func getAllImages(db *cl.DB) func(c *gin.Context) {
