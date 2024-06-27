251e1ade3ec3654b4c47445e9e86b8a248adde10
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2024-06-27 17:15:37 +0330

    refactor: add user_id to Image struct

--- internal/pkg/types.go
+++ internal/pkg/types.go
@@ -6,5 +6,14 @@ type Image struct {
 	UUID      string    `clover:"_id" json:"uuid"`
 	Url       string    `clover:"url" json:"image_url"`
 	Likes     int64     `clover:"likes" json:"likes"`
+	UserId    string    `clover:"user_id" json:"user_id"`
 	CreatedAt time.Time `clover:"created_at" json:"created_at"`
+}
+
+type User struct {
+	UUID       string    `clover:"_id" json:"uuid"`
+	Username   string    `clover:"url" json:"username"`
+	Fullname   string    `clover:"fullname" json:"fullname"`
+	Passphrase string    `clover:"passphrase" json:"-"`
+	CreatedAt  time.Time `clover:"created_at" json:"created_at"`
 }
